16cf1943d464b54b95052b60b22f5267a76433f2
FranMacedo <franciscomacedo@lisboaenova.org>
2020-04-24 19:13:19 +0100

    Updates

--- precache-manifest.2e319e2aa6e5172716df7f9d0869a05e.js
+++ precache-manifest.2e319e2aa6e5172716df7f9d0869a05e.js
@@ -16,16 +16,16 @@ self.__precacheManifest = [
     "url": "/myreads/static/js/runtime~main.1b7e853e.js"
   },
   {
-    "revision": "9d4e3f366f039249695de85a30a02b95",
-    "url": "/myreads/static/media/arrow-drop-down.9d4e3f36.svg"
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/myreads/static/media/add.75d390e5.svg"
   },
   {
     "revision": "20e8847d1eb7759c19b0e30beb8849cf",
     "url": "/myreads/static/media/arrow-back.20e8847d.svg"
   },
   {
-    "revision": "75d390e5eabc3254418e950df631389b",
-    "url": "/myreads/static/media/add.75d390e5.svg"
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/myreads/static/media/arrow-drop-down.9d4e3f36.svg"
   },
   {
     "revision": "d0276f408d955fd0f3390ab0e349365c",
